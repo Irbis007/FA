62f59ecdeda0302edb341a18a3b20c9fa0315361
Irbis007 <suskodomk@gmail.com>
2024-03-09 16:18:03 +0300

    commit preloader and commit one js import strok

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-window.addEventListener("load", function (e) {
-  const preloader = document.querySelector(".preloader");
-  preloader.style.animation = "preloaderAnim 0.7s 1s forwards";
-  const body = document.querySelector("body");
-  setInterval(() => {
-    body.style.overflow = "auto";
-  }, 1700);
-});
+// window.addEventListener("load", function (e) {
+//   const preloader = document.querySelector(".preloader");
+//   preloader.style.animation = "preloaderAnim 0.7s 1s forwards";
+//   const body = document.querySelector("body");
+//   setInterval(() => {
+//     body.style.overflow = "auto";
+//   }, 1700);
+// });
 
 const faqs = document.querySelectorAll(".faq .faq__body .item");
 
